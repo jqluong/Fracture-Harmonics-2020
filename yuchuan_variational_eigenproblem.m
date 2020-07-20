a1c01739075ce471436dc7fb4d0b39b661fb055c
Yuchuan Yang <55909431+yyuchuan@users.noreply.github.com>
2020-07-20 23:36:26 +0800

    included plot of first 5 eigenfunctions

--- yuchuan_variational_eigenproblem.m
+++ yuchuan_variational_eigenproblem.m
@@ -1,10 +1,8 @@
 %% Input data
 a = 0; %left endpoint
 b = 1; %right endpoint
-n = 10; %number of intervals/size of partition
-h = 1/n; %interval length (assume evenly-partitioned)
-f_a = 0; %left boundary condition f(a) = 0
-f_b = 1; %right boundary condition f(b) = 1
+n = 1000; %number of intervals/size of partition
+h = (b-a)/n; %interval length (assume evenly-partitioned)
 
 
 
@@ -25,9 +23,19 @@ eigfunction = (1/sqrt(h))*V(:,1);
 
 
 
+
 %% Plot graph of y = f(x)
 
 x = linspace(a,b,n+1);
-plot(x,eigfunction);
-axis([0 1 -2 2])
 
+
+
+%% Plot first 5 eigenfunctions
+
+for i = 1:5
+    plot (x,(1/sqrt(h))*V(:,i));
+    axis([0 1 -2 2])
+    hold on
+end
+
+legend('eigenfunction 1','eigenfunction 2','eigenfunction 3','eigenfunction 4','eigenfunction 5');
