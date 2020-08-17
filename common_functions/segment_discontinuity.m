10f1b3c4d918c9e4a6f0c57715e54323d1df6aff
akr97 <aravind947@gmail.com>
2020-08-17 02:15:32 -0400

    fixed bug

--- common_functions/segment_discontinuity.m
+++ common_functions/segment_discontinuity.m
@@ -8,5 +8,5 @@ function D = segment_discontinuity(n)
     % discontinuities as elements:
     %     [ y_{2,3}-y_{2,1}; y_{3,4}-y_{3,2}; ... ; y_{n,n+1}-y_{n,n-1} ]
     
-    D = [sparse(1:n-1,2:n,1,n-1,n),-speye(3,4)];
+    D = [sparse(1:n-1,2:n,1,n-1,n),-speye(n-1,n)];
 end
