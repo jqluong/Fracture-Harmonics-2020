c779c846484deac485690d139b648985e656880c
Yuchuan Yang <55909431+yyuchuan@users.noreply.github.com>
2020-08-23 00:43:44 +0800

    unit norm constraint (eigenmodes)

--- Yuchuan/2D Problem/poisson_2D.m
+++ Yuchuan/2D Problem/poisson_2D.m
@@ -1,42 +1,54 @@
-%% Example of solving Possion equation with Dirichlet boundary condition
-%requires poisson_2D.m
+%% Solves the 2D Poisson equation: Laplace u = f (over triangular mesh in R^2) subject to Dirichlet boundary condition
+%requires gptoolbox
+%INPUTS:
+    %V = vertices
+    %F = faces
+    %f = source term of Poisson equation
+    %u_0 = boundary condition
+    %bv = boundary vertices
+    
+%OUTPUT:
+    %u_int = vector of values of u in the interior of domain
 
-clear
+function [u_sol] = poisson_2D(V,F,f,u_0,bv)
 
+  % Based on 2.1 of "Algorithms and Interfaces for Real-Time Deformation of 2D and 3D
+  % shapes" [Jacobson 2013]
+  
+  
+  %if nargin == 5, this is a boundary condition problem
 
-%% Generate mesh
-[x, y] = meshgrid(1:10, 1:10);
-points = [x(:), y(:)];
-V = [points zeros(size(points,1),1)];
-F = delaunay(points);
+  if nargin == 5
+      L = cotmatrix(V,F); %Laplacian operator (sparse)
+      M = massmatrix(V,F,'full'); %mass matrix (sparse)
 
+      b_1 = M*f;
+      b_1(bv) = [];
 
-%% Input
-n = size(V,1);
-f = sparse(n,1); %source term in Poisson equation
+      b_2 = (-1)*L*u_0;
+      b_2(bv) = [];
 
-%% Create vector for boundary values u_0
-O = outline(F);     %boundary outline (edges)
-bv = unique(O(:));  %boundary vertices
-v = zeros(n,1);
-
-for i = bv               %%this is for the boundary condition of u(x,y) = x + y along the boundary
-    v(i) = V(i,1)+V(i,2);
-end 
-
-u_0 = v;
-
-
-%% Solve for u
-u_int = poisson_2D(V,F,f,u_0,bv);
-v_int = setdiff((1:size(V,1)),bv).'; %interior vertices
-s = sparse(size(V,1),1);
-s(v_int) = u_int;
-u = s + u_0;
+      b = b_1 + b_2;
 
 
-%% Plot u
-X = reshape(V(:,1),10,10)';
-Y = reshape(V(:,2),10,10)';
-Z = reshape(u,10,10)';
-surf(X,Y,Z);
+      L(bv,:) = [];
+      L(:,bv) = [];
+
+      u_sol = L\b; 
+
+  
+  %if nargin == 3, this is a unit norm constraint problem ||u|| = 1    
+  elseif nargin == 3
+      L = cotmatrix(V,F); %Laplacian operator (sparse)
+      M = massmatrix(V,F,'full'); %mass matrix (sparse)
+      G  = grad(V,F);
+      dblA = doublearea(V,F);
+      GMG = G'*repdiag(diag(sparse(dblA)/2),size(V,2))*G;
+      %H = inv(diag(sparse(dblA)/2))*GMG;   %how to implement area matrix??
+      [eiv,~] = eig(full(GMG));
+      u_sol = eiv;
+      
+  end
+
+end
+
