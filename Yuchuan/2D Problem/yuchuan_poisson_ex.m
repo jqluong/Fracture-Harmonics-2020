fb78e675da4b06a3ae6b65cabfa71123718800c5
Yuchuan Yang <55909431+yyuchuan@users.noreply.github.com>
2020-08-23 00:44:47 +0800

    unit norm constraint (eigenmodes)

--- Yuchuan/2D Problem/yuchuan_poisson_ex.m
+++ Yuchuan/2D Problem/yuchuan_poisson_ex.m
@@ -15,20 +15,49 @@ F = delaunay(points);
 n = size(V,1);
 f = sparse(n,1); %source term in Poisson equation
 
-%% Create vector for boundary values u_0
-O = outline(F);     %boundary outline (edges)
-bv = unique(O(:));  %boundary vertices
-v = zeros(n,1);
-
-for i = bv
-    v(i) = 100;
-end 
-
-u_0 = v;
 
 
-%% Solve for u
-u_int = poisson_2D(V,F,f,u_0);
-u = u_int + u_0;
 
 
+%% 1. Boundary condition problem
+
+% % Create vector for boundary values u_0
+% O = outline(F);     %boundary outline (edges)
+% bv = unique(O(:));  %boundary vertices
+% v = zeros(n,1);
+% 
+% for i = bv               %%this is for the boundary condition of u(x,y) = x + y along the boundary
+%     v(i) = V(i,1)+V(i,2);
+% end 
+% 
+% u_0 = v;
+% 
+% % Solve for u
+% u_int = poisson_2D(V,F,f,u_0,bv);
+% v_int = setdiff((1:size(V,1)),bv).'; %interior vertices
+% s = sparse(size(V,1),1);
+% s(v_int) = u_int;
+% u = s + u_0;
+
+
+
+
+
+%% 2. Unit norm constraint problem
+
+u = poisson_2D(V,F,f);
+
+
+
+
+
+
+
+%% Plot u
+
+
+X = reshape(V(:,1),10,10)';
+Y = reshape(V(:,2),10,10)';
+%Z = reshape(u,10,10)';
+Z = reshape(u(:,5),10,10)';   %u(:,i) = i_th eigen mode corresponding to i_th smallest eigenvalue
+surf(X,Y,Z);
