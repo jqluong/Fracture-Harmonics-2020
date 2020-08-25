e4bdf9cd69505314dd56828940d821f96bac74c1
jqluong <jackaham_luongcoln@mail.fresnostate.edu>
2020-08-25 11:15:54 -0700

    works slightly better now, flat
    Works slightly better now, but the plot is flat for some reason

--- common_functions/plotFaceFunction.m
+++ common_functions/plotFaceFunction.m
@@ -1,7 +1,10 @@
 function plotFaceFunction(V, F, faceFunction)
     [m,~] = size(faceFunction);
+    figure(1)
+    hold on
     for i = 1:m
         facePiece = faceFunction(i,:);
-        trisurf(F(i,:), V(F(i,:), 1), V(F(i,:), 2), facePiece(:));
+        trisurf([1 2 3], V(F(i,:), 1), V(F(i,:), 2), facePiece(:));
     end
+    hold off
 end
