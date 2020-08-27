b3c2ce0a5445adf032c0cbfccafe9e7049e2cd2f
akr97 <aravind947@gmail.com>
2020-08-27 00:45:08 -0400

    changed gradient matrix so that face ordering is preserved

--- common_functions/face_functions/face_dirichlet_energy.m
+++ common_functions/face_functions/face_dirichlet_energy.m
@@ -1,13 +1,20 @@
-function E = face_dirichlet_energy(V, F, u)
+function E = face_dirichlet_energy(V, F, u, G)
     % compute the Dirichlet Energy of a face defined function u
-    % inputs: Mesh V, F, and function u
+    % inputs: Mesh V, F, function u, optional gradient G
     % u is represented as:
     %   u = [ u(F1_v1) u(F1_v2) u(F1_v3) u(F2_v1) u(F2_v2) u(F2_v3) ... ]'
     % output is the energy
     
+    if nargin == 3 % gradient matrix not provided
+        du = face_grad(V, F)*u;
+    elseif nargin == 4 % gradient matrix provided
+        du = G*u;
+    else
+        error('check all input arguments provided');
+    end
+    
     A = doublearea(V, F);
-    du = face_grad(V, F)*u;
-    du = reshape(du, [], size(V,2)); % reshape du from dim*F x 1 to F x dim
+    du = reshape(du, size(V,2), [])'; % reshape du from dim*F x 1 to F x dim
     du = sum(du.^2, 2); % norm square of each gradient vector
     
     E = sum(A.*du)/4;
