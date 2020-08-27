748eba58e89247e7a5d3feb8abb328d740f2d351
akr97 <aravind947@gmail.com>
2020-08-27 02:17:40 -0400

    updated continuity code to use sparse

--- common_functions/face_functions/face_discontinuity_matrix.m
+++ common_functions/face_functions/face_discontinuity_matrix.m
@@ -1,5 +1,4 @@
 %% Computes discontinuity operator D 
-
 %requires gptoolbox
 
 %INPUTS:
@@ -7,68 +6,52 @@
 %OUTPUTS:
     %D = |E|-by-3|F| sparse matrix , E = #edges
                                 
+function D = face_discontinuity_matrix_local(F)
+    temp = F';
+    f_vert = temp(:);
+    E = edges(F); %Edges
+    ET = edge_triangle_adjacency(F,E);
+    lE = length(E);
+    c = zeros(size(E,2),2);
+    d = zeros(size(E,2),2);
 
-
-function [D] = discontinuity_matrix(F)
-
-temp = F';
-f_vert = temp(:);
-E = edges(F); %Edges
-ET = edge_triangle_adjacency(F,E);
-c = zeros(size(E,2),2);
-d = zeros(size(E,2),2);
-
-for i = 1:size(E,1)
-    if ET(i,2) == -1
-        c(i,:) = -1;
-        d(i,:) = -1;
-    else 
-        for j = 0:2
-            if f_vert(3*ET(i,1)-j)== E(i,1)
-                c(i,1) = 3*ET(i,1)-j;
-            elseif f_vert(3*ET(i,1)-j)== E(i,2)
-                c(i,2) = 3*ET(i,1)-j;
+    for i = 1:lE
+        if ET(i,2) == -1
+            c(i,:) = -1;
+            d(i,:) = -1;
+        else 
+            for j = 0:2
+                if f_vert(3*ET(i,1)-j)== E(i,1)
+                    c(i,1) = 3*ET(i,1)-j;
+                elseif f_vert(3*ET(i,1)-j)== E(i,2)
+                    c(i,2) = 3*ET(i,1)-j;
+                end
             end
-        end
         
-        for k = 0:2
-            if f_vert(3*ET(i,2)-k)== E(i,1)
-                d(i,1) = 3*ET(i,2)-k;
-            elseif f_vert(3*ET(i,2)-k)== E(i,2)
-                d(i,2) = 3*ET(i,2)-k;
-            end
-        end 
-    end
-end
-    
-    
-D = zeros(size(E,1),length(f_vert));
-    
-for m = 1:size(E,1)
-    if c(m,1) <= 0
-        continue;
-    else 
-        D(m,c(m,1)) = 1/2;
-        D(m,c(m,2)) = 1/2;
-        D(m,d(m,1)) = -1/2;
-        D(m,d(m,2)) = -1/2;
+            for k = 0:2
+                if f_vert(3*ET(i,2)-k)== E(i,1)
+                    d(i,1) = 3*ET(i,2)-k;
+                elseif f_vert(3*ET(i,2)-k)== E(i,2)
+                    d(i,2) = 3*ET(i,2)-k;
+                end
+            end 
+        end
     end
     
-end
     
-D = sparse(D);
-    
+    %D = zeros(size(E,1),length(f_vert));
+    Dpos = zeros(4*lE, 3);
+    ii = 1;
+    for m = 1:size(E,1)
+        if c(m,1) <= 0
+            continue;
+        else
+            Dpos_c = [m c(m,1) 1/2; m c(m,2) 1/2; m d(m,1) -1/2; m d(m,2) -1/2];
+            Dpos(ii:ii+3,:) = Dpos_c;
+        end
+        ii = ii+4;
+    end
+    ii = ii-1;
+
+    D = sparse(Dpos(1:ii,1), Dpos(1:ii,2), Dpos(1:ii,3), lE,length(f_vert));
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
