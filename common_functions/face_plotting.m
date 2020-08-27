63f43dc81588d5fa84cac35c39797afa9b4d972c
jqluong <jackaham_luongcoln@mail.fresnostate.edu>
2020-08-27 10:41:54 -0700

    Fixed face plotting function to work in 3D!

--- common_functions/face_plotting.m
+++ common_functions/face_plotting.m
@@ -2,10 +2,13 @@ function plotFaceFunction(V, F, faceFunction)
     %face function is F x 3
     [m,~] = size(faceFunction);
     figure(1)
-    hold on
     for i = 1:m
+        if i == 1
+            hold on
+        end
         facePiece = faceFunction(i,:);
         trisurf([1 2 3], V(F(i,:), 1), V(F(i,:), 2), facePiece(:));
     end
+    colorbar
     hold off
 end
