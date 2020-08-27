84b8e904a1e197bf71105296324c02867af7c05a
yuchuan <yangyuchuan@ucla.edu>
2020-08-27 13:37:11 +0800

    moved functions

---
+++ Yuchuan/2D Problem/grad_segment.m
@@ -0,0 +1,20 @@
+%% Computes a gradient OPERATOR G for discontinuously-stored functions over tri mesh
+
+%INPUTS: 
+    %V = vertices
+    %F = faces
+%OUTPUTS:
+    %G = #3|F|-by-3|F| matrix 
+%e.g. G*u = gradient vector
+
+function [G] = grad_segment(V,F)
+
+
+
+
+
+
+
+
+
+end
