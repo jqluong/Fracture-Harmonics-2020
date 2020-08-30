a122db9b27c38c27bc473055db9cbad00d55d37e
lmattos-11 <lmattos@g.ucla.edu>
2020-08-29 19:22:07 -0700

    testing new quadprog function

---
+++ Leticia/2D/test_segmented_faces_quadprog.m
@@ -0,0 +1,12 @@
+
+% generate circle
+s = 100;
+theta = [0:2*pi()/s:2*pi()]';
+x = cos(theta);
+y = sin(theta);
+
+[V, F] = triangle([x,y], 'Quality', 30, 'MaxArea', 0.001);
+
+u = laplace_eq_2D_seg_quadprog(V,F);
+
+face_plot(V,F,u)
