1136d6250d25b96a0f7624e1d73e3d8a4ed6082c
lmattos-11 <lmattos@g.ucla.edu>
2020-08-29 21:38:39 -0700

    finished code, problem non-convex due to non-symetry of Hessian generated by faces_grad(V,F)

--- Leticia/2D/test_segmented_faces_quadprog.m
+++ Leticia/2D/test_segmented_faces_quadprog.m
@@ -7,6 +7,12 @@ y = sin(theta);
 
 [V, F] = triangle([x,y], 'Quality', 30, 'MaxArea', 0.001);
 
+[m,~] = size(V);
+
+V_Z = zeros(m,1);
+
+V = [ V V_Z ]; 
+
 u = laplace_eq_2D_seg_quadprog(V,F);
 
 face_plot(V,F,u)
