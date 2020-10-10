1da332675156c741d657ffb37eb9ceb11a25a4e5
lmattos-11 <lmattos@g.ucla.edu>
2020-10-10 14:43:52 -0700

    compiled 'common function' based on our pseudo-code for L eigenmodes of vertex functiosn, created 'mesh functions' to call to create commonly used meshes

--- Leticia/2D/test_segmented_faces_quadprog.m
+++ Leticia/2D/test_segmented_faces_quadprog.m
@@ -1,17 +1,27 @@
+<<<<<<< Updated upstream
 % generate circle
 s = 100;
 theta = [0:2*pi()/s:2*pi()]';
 x = cos(theta);
 y = sin(theta);
+=======
+% construct mesh
+f = 10;
+x = 0:1/f:1;
+y = 0:1/f:1;
 
-[V, F] = triangle([x,y], 'Quality', 30, 'MaxArea', 0.001);
+[X,Y] = meshgrid(x,y);
+>>>>>>> Stashed changes
+
+V = [X(:), Y(:)];
+
+F = delaunay(V);
 
 [m,~] = size(V);
 
 V_Z = zeros(m,1);
 
-V = [ V V_Z ]; 
 
-u = laplace_eq_2D_seg_quadprog(V,F);
+u = laplace_eq_2D_seg_quadprog([ V V_Z ],F);
 
 face_plot(V,F,u)
